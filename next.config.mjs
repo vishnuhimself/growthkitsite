d986fa135fe13176f8e8541d99ad21b832ae3896
Srivishnu Ramakrishnan <dreams.sri@gmail.com>
2025-02-08 14:57:59 +0530

    Serve unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true, // This will serve original images without optimization
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'growthkitapp.com',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
